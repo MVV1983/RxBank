2faeb29b12f25c271dac43d781a947b3d6d3c5ee
v.matyukhin <silf083@gmail.com>
2021-06-17 19:18:00 +0700

    add permitions

--- app/src/main/java/com/example/rxbank/view/CongratulationFragment.kt
+++ app/src/main/java/com/example/rxbank/view/CongratulationFragment.kt
@@ -5,12 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
-
 import com.example.rxbank.R
-import kotlinx.android.synthetic.main.fragment_congratulation.*
-import kotlinx.android.synthetic.main.fragment_login.*
-import kotlinx.android.synthetic.main.fragment_login.registrationButton
 
 class CongratulationFragment : Fragment() {
 
@@ -21,13 +16,5 @@ class CongratulationFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_congratulation, container, false)
 
-
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        back_to_Login?.setOnClickListener(
-            Navigation.createNavigateOnClickListener(R.id.action_congratulationFragment_to_loginFragment)
-        )
     }
 }
--- app/src/main/java/com/example/rxbank/view/RegistrationFragment.kt
+++ app/src/main/java/com/example/rxbank/view/RegistrationFragment.kt
@@ -1,14 +1,13 @@
 package com.example.rxbank.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
-import androidx.navigation.Navigation
-
 import com.example.rxbank.R
 import com.example.rxbank.api.RetrofitService
 import com.example.rxbank.data.requests.LoginRequest
@@ -20,7 +19,7 @@ import kotlinx.android.synthetic.main.fragment_login.registrationButton
 import kotlinx.android.synthetic.main.fragment_registration.*
 
 class RegistrationFragment : Fragment() {
-    
+
     var disposable: Disposable? = null
 
     private var isCorrectNameInput: Boolean = false
@@ -33,12 +32,13 @@ class RegistrationFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_registration, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         registrationButton?.setOnClickListener {
-            val user = LoginRequest("silf","123")
-            //val user = RegistrationData(registrationUser.text.toString(), registrationPassword.text.toString())
+            val user = LoginRequest("mirror", "12345")
+            //val user = LoginRequest(registrationName.text.toString(), registrationPass.text.toString())
             postRegistration(user)
         }
 
@@ -68,28 +68,25 @@ class RegistrationFragment : Fragment() {
 
 
     // POST new RegistrationData
-    private fun postRegistration(registrationData: LoginRequest) {
+    private fun postRegistration(user: LoginRequest) {
 
         val clientR = RetrofitService.create()
-
-        disposable = clientR.postRequest(registrationData)
+        Log.v("test", "$user")
+        disposable = clientR.postRequest(user)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
-
             .subscribe(
                 { response -> onResponse(response) },
-                //{ result -> Log.v("POSTED ARTICLE", "" + registrationUser) },
-                {  t -> onFailure(t) }
+                { t -> onFailure(t) }
             )
     }
 
-    private fun onResponse(response: LoginResponse){
-        Toast.makeText(context, response.name+ response.role, Toast.LENGTH_LONG).show()
-        Navigation.createNavigateOnClickListener(R.id.action_registrationFragment_to_congratulationFragment)
+    private fun onResponse(response: LoginResponse) {
+        Toast.makeText(context, response.name + response.role, Toast.LENGTH_LONG).show()
     }
 
     private fun onFailure(t: Throwable) {
-        Toast.makeText(context,t.message, Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
     }
 
     private fun allowRegistration() {
